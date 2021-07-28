9da8bedcc9faf05a3f13db46a082e35219040f09
pham dinh <BO-DINH@BO-DINH>
2021-07-28 22:07:16 +0700

    [FE] message-UI-done.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -4,6 +4,32 @@ import { Menu, Dropdown, Button, Space } from 'antd';
 import HeaderButtonsDesktopComponent from "./HeaderButtonsDesktopComponent";
 
 function Layout(props) {
+    useEffect(()=>{
+        const toggleThemeBtn = document.querySelector('.header__theme-button');
+        document.onload = setInitialTheme(localStorage.getItem('theme'));
+        function setInitialTheme(themeKey) {
+            if(themeKey === 'dark') {
+                document.documentElement.classList.add('darkTheme');
+            }
+            else {
+                document.documentElement.classList.remove('darkTheme');
+            }
+        }
+
+// Toggle theme button
+        toggleThemeBtn.addEventListener('click', () => {
+            // Toggle root class
+            document.documentElement.classList.toggle('darkTheme');
+
+            // Saving current theme on LocalStorage
+            if(document.documentElement.classList.contains('darkTheme')) {
+                localStorage.setItem('theme', 'dark');
+            }
+            else {
+                localStorage.setItem('theme', 'light');
+            }
+        });
+    })
     return(
         <div className='instagram-home-page-wrap'>
             <header className="header">
---
+++ src/screens/MessagePage.js
@@ -0,0 +1,12 @@
+import {connect} from "react-redux";
+import MessageComponent from "../components/message/MessageComponent";
+
+function mapStateToProps(state) {
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(MessageComponent)
