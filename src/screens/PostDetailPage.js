c90b73d2451ffac22a7c7b352d2eb43676f8bf0b
pham dinh <BO-DINH@BO-DINH>
2021-08-06 13:32:26 +0700

    [FE]post-detail-page UI-DONE.

---
+++ src/screens/PostDetailPage.js
@@ -0,0 +1,11 @@
+import {connect} from "react-redux";
+import PostDetailComponentPage from "../components/detail-post/PostDetailComponentPage";
+
+function mapStateToProps(state) {
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+export default connect(mapStateToProps,mapDispatchToProps)(PostDetailComponentPage)
