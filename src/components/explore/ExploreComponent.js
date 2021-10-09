4589482eb61f461271ad6c703008513bb0ce6771
pham dinh <BO-DINH@BO-DINH>
2021-10-09 21:23:18 +0700

    [FE] explore_page_UI.

---
+++ src/components/explore/ExploreComponent.js
@@ -0,0 +1,193 @@
+import React from "react";
+import {BsFillHeartFill, FaComment} from "react-icons/all";
+
+function ExploreComponent() {
+    return(
+        <div className="wrap-body-page-explore">
+            <div className="wrap-center-body-page-explore">
+                <div className="body-page-explore">
+                    <div className="body-page-explore-item-3">
+                        <div className="item">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1633527993/instargram-clone/f8tkzvuwq2zooplzlmh3.jpg" />
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="body-page-explore-item-3">
+                        <div className="item">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="body-page-explore-item-2">
+                        <div className="item-large">
+                            <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                            <div className="wrap-post-info">
+                                <div className="post-info">
+                                    <div className="likes">
+                                        <div className="L1L">
+                                            <BsFillHeartFill />
+                                        </div>
+                                        10
+                                    </div>
+                                    <div className="comments">
+                                        <div className="C1C">
+                                            <FaComment />
+                                        </div>
+                                        10
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="item-2">
+                            <div className="item-21">
+                                <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                                <div className="wrap-post-info">
+                                    <div className="post-info">
+                                        <div className="likes">
+                                            <div className="L1L">
+                                                <BsFillHeartFill />
+                                            </div>
+                                            10
+                                        </div>
+                                        <div className="comments">
+                                            <div className="C1C">
+                                                <FaComment />
+                                            </div>
+                                            10
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item-21">
+                                <img src="https://res.cloudinary.com/dinhpv/image/upload/v1632041022/instargram-clone/flj97yl9cj9ydcmhppak.jpg"/>
+                                <div className="wrap-post-info">
+                                    <div className="post-info">
+                                        <div className="likes">
+                                            <div className="L1L">
+                                                <BsFillHeartFill />
+                                            </div>
+                                            10
+                                        </div>
+                                        <div className="comments">
+                                            <div className="C1C">
+                                                <FaComment />
+                                            </div>
+                                            10
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ExploreComponent
---
+++ src/screens/ExplorePage.js
@@ -0,0 +1,11 @@
+import {connect} from "react-redux";
+import ExploreComponent from "../components/explore/ExploreComponent";
+
+function mapStateToProps(state) {
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+export default connect(mapStateToProps,mapDispatchToProps)(ExploreComponent)
