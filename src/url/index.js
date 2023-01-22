d212c147d7c78df9e1823f65751209769d2645a9
DINH <phamvandinhmta@gmail.com>
2023-01-22 09:42:25 +0700

    deploy branch develop

--- src/url/index.js
+++ src/url/index.js
@@ -1,5 +1,7 @@
-export const BASE_URL='http://localhost:8080'
-export const BASE_URL_WEBSOCKET='http://localhost:8080/api/v1'
+// export const BASE_URL='http://localhost:8080'
+export const BASE_URL='https://be-meta.herokuapp.com'
+// export const BASE_URL_WEBSOCKET='http://localhost:8080/api/v1'
+export const BASE_URL_WEBSOCKET='https://be-meta.herokuapp.com/api/v1'
 /* https://api.cloudinary.com/v1_1/<cloud name>/<resource_type>/upload
  resource_type : auto (image,video,...) */
 export const API_CLOUDINARY_BASE_URL='https://api.cloudinary.com/v1_1/dinhpv/auto/upload'
